ebb7712907fe648590895b67761712119c420aba
Abhinav-Modi <abhinavmodi07@gmail.com>
2022-02-03 13:28:30 +0530

    first

---
+++ src/notes.js
@@ -0,0 +1,7 @@
+const notes = [
+	{ key: 1, title: "Note 1", body: "This is note 1" },
+	{ key: 2, title: "Note 2", body: "This is note 2" },
+	{ key: 3, title: "Note 3", body: "This is note 3" },
+	{ key: 4, title: "Note 4", body: "This is note 4" },
+];
+export default notes;
